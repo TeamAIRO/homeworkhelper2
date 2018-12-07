798b972b1e90bbdc5d1aca8ede22698261277b36
TeamAIRO <45614926+TeamAIRO@users.noreply.github.com>
2018-12-07 17:54:52 -0500

    Update index.js

--- index.js
+++ index.js
@@ -4,9 +4,18 @@ const express = require("express");
 const bodyParser = require("body-parser");
 
 /* chirayu add */
+
 const fs = require('fs');
 const readline = require('readline');
 const {google} = require('googleapis');
+// If modifying these scopes, delete token.json.
+//const SCOPES = ['https://www.googleapis.com/auth/calendar.readonly'];
+const SCOPES = ['https://www.googleapis.com/auth/calendar'];
+// The file token.json stores the user's access and refresh tokens, and is
+// created automatically when the authorization flow completes for the first
+// time.
+const TOKEN_PATH = 'token.json';
+
 /* chirayu add */
 
 const restService = express();
