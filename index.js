6bc07672430e5b8441987d0b6110cbb3310fe689
TeamAIRO <45614926+TeamAIRO@users.noreply.github.com>
2018-12-07 18:01:43 -0500

    Update index.js

--- index.js
+++ index.js
@@ -26,6 +26,25 @@ fs.readFile('credentials.json', (err, content) => {
   authorize(JSON.parse(content), writeEvents);
 });
 
+/**
+ * Create an OAuth2 client with the given credentials, and then execute the
+ * given callback function.
+ * @param {Object} credentials The authorization client credentials.
+ * @param {function} callback The callback to call with the authorized client.
+ */
+function authorize(credentials, callback) {
+  const {client_secret, client_id, redirect_uris} = credentials.installed;
+  const oAuth2Client = new google.auth.OAuth2(
+      client_id, client_secret, redirect_uris[0]);
+
+  // Check if we have previously stored a token.
+  fs.readFile(TOKEN_PATH, (err, token) => {
+    if (err) return getAccessToken(oAuth2Client, callback);
+    oAuth2Client.setCredentials(JSON.parse(token));
+    callback(oAuth2Client);
+  });
+}
+
 /* chirayu add */
 
 const restService = express();
