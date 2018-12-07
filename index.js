e6d9416cee4ef61a4e83208ef2b5c1e7dc8878e8
TeamAIRO <45614926+TeamAIRO@users.noreply.github.com>
2018-12-07 18:24:20 -0500

    Update index.js

--- index.js
+++ index.js
@@ -206,6 +206,20 @@ restService.post("/echo", function(req, res) {
     req.body.queryResult.parameters.firstName 
       ? req.body.queryResult.parameters.firstName + " " + req.body.queryResult.parameters.lastName
       : "Seems like some problem. Speak again.";
+  
+  /*** chirayu add */
+  // Load client secrets from a local file.
+fs.readFile('credentials.json', (err, content) => {
+  if (err) return console.log('Error loading client secret file:', err);
+  // Authorize a client with credentials, then call the Google Calendar API.
+ 
+  //authorize(JSON.parse(content), listEvents);
+
+  authorize(JSON.parse(content), writeEvents);
+});
+  
+/*chirayu add */
+  
   return res.json({
     fulfillmentText: speech,
     source: "webhook-echo-sample"
