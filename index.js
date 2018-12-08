d6b0a236aa08120da11b47dd51cf88d4d0533dd7
TeamAIRO <45614926+TeamAIRO@users.noreply.github.com>
2018-12-08 14:46:05 -0500

    Update index.js

--- index.js
+++ index.js
@@ -101,7 +101,7 @@ function writeEvents(auth) {
   var today = new Date();
   
 var event = {
-  'summary': subject1 + ,
+  'summary': subject1
   'location': '800 Howard St., San Francisco, CA 94103',
   'description': 'A chance to hear more about Google\'s developer products.',
   var date;
