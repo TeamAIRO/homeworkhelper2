96d32cab6c3f0b59c21fc4c29a5847076887de85
TeamAIRO <45614926+TeamAIRO@users.noreply.github.com>
2019-10-31 10:05:14 -0400

    Update index.js

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ const SCOPES = ['https://www.googleapis.com/auth/calendar'];
 // The file token.json stores the user's access and refresh tokens, and is
 // created automatically when the authorization flow completes for the first
 // time.
-//const TOKEN_PATH = 'token.json';
+const TOKEN_PATH = 'a';
 
 
 
